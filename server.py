3d907bf9236e5c121a9202f29ede117df71f9007
Tim Schmidt <tschmidt@lazyelephantstudio.com>
2018-02-13 21:09:49 -0500

    Cleaned up server.py a bit, moved some functions to utils.py, and removed unused js file

--- server.py
+++ server.py
@@ -1,38 +1,18 @@
-from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 import numpy as np
-from flask import Flask, request, jsonify, abort, render_template
-from utils import thai_text_to_vec
+from flask import Flask, request, abort, render_template
+from utils import predictions_to_text, prepare_text
 
 app = Flask(__name__)
 model = load_model('Im_a_model_you_know_what_I_mean.h5')
 model._make_predict_function()
 char_to_int = np.load('char_to_int.npy').item()
 int_to_char = np.load('int_to_char.npy').item()
-
-def prepare(text):
-    """
-    Accepts a string of Thai text then:
-        - Splits around any whitespace
-        - Pad each split sentence to 128 characters
-    """
-    global char_to_int
-    sequences = [thai_text_to_vec(seq, char_to_int) for seq in text]
-    sequences = pad_sequences(sequences, maxlen=128, padding='post', truncating='post')
-    return sequences
-
-def predictions_to_text(samples, predictions):
-    text_with_spaces = []
-    for i, text in enumerate(samples):
-      for j, char in enumerate(text):
-        temp = char if predictions[i,j] == 0 else " " + char
-        text_with_spaces.append(temp)
-
-    return "".join(text_with_spaces).lstrip()
   
 def predict(original_text):
+    global char_to_int
     split_text = original_text.split()
-    prepared = prepare(split_text)
+    prepared = prepare_text(split_text, char_to_int)
     predictions = model.predict(prepared)
     predictions[predictions > 0.7] = 1
     predictions[predictions <= 0.7] = 0
@@ -49,4 +29,4 @@ def index():
     return render_template("index.html", tokenized=tokenized)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- utils.py
+++ utils.py
@@ -1,6 +1,27 @@
+from keras.preprocessing.sequence import pad_sequences
+
 def thai_text_to_vec(text, char_to_int, num_classes=None):
 
   if num_classes is None:
     num_classes = len(char_to_int)
 
   return [char_to_int[char] for char in text]
+
+def predictions_to_text(samples, predictions):
+    text_with_spaces = []
+    for i, text in enumerate(samples):
+      for j, char in enumerate(text):
+        temp = char if predictions[i,j] == 0 else " " + char
+        text_with_spaces.append(temp)
+
+    return "".join(text_with_spaces).lstrip()
+
+def prepare_text(text, char_to_int):
+    """
+    Accepts a list of Thai text strings then:
+        - Converts each string to an integer array
+        - Pad each array to 128 characters
+    """
+    sequences = [thai_text_to_vec(seq, char_to_int) for seq in text]
+    sequences = pad_sequences(sequences, maxlen=128, padding='post', truncating='post')
+    return sequences
