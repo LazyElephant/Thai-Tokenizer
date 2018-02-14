b02390250e3d1147e5fc40537f8357463fa11e0d
LazyElephant <tschmidt@lazyelephantstudio.com>
2018-02-14 18:03:58 -0500

    Add styling, restful endpoint, and javascript to consume the endpoint

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from keras.models import load_model
 import numpy as np
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template, jsonify
 from utils import predictions_to_text, prepare_text
 
 app = Flask(__name__)
@@ -19,12 +19,24 @@ def predict(original_text):
     tokenized = predictions_to_text(split_text, predictions)
     return tokenized
             
+@app.route("/predict", methods=["POST"])
+def api_predict():
+    data = request.get_json()
+    if data and 'text' in data:
+        request.headers['Access-Control-Allow-Origin'] = '*'
+        tokenized = predict(data['text'])
+        return jsonify({'tokenized': tokenized})
+    else:
+        return jsonify({'error': 400, 'message': 'Bad Request'})
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     tokenized = None
     if request.method == "POST":
-        if request.form and "input" in request.form:
-            tokenized = predict(request.form["input"])
+        if request.form and "text" in request.form:
+            tokenized = predict(request.form["text"])
+        else:
+            abort(400)
 
     return render_template("index.html", tokenized=tokenized)
 
---
+++ static/main.js
@@ -0,0 +1,37 @@
+const gridContainer = document.querySelector('.grid-container')
+
+function displayResponse(text) {
+  let div = document.querySelector('.tokenized')
+
+  if (!div) {
+    div = document.createElement('div')
+    div.classList.add('tokenized')
+    gridContainer.appendChild(div)
+  }
+  div.innerText = text
+}
+
+async function handleSubmit(evt) {
+  evt.preventDefault()
+  const textArea = evt.target['text']
+  const body = JSON.stringify({ text: textArea.value })
+
+  const response = await fetch('http://localhost:5000/predict', {
+    body,
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+  const json = await response.json()
+
+  if ('tokenized' in json) {
+    displayResponse(json.tokenized)
+  }
+  
+  textArea.focus()
+}
+
+document
+  .forms['thai-form']
+  .addEventListener('submit', handleSubmit)
